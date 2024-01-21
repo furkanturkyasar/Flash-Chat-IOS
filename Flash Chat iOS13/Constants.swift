13465bf2b695cfbf68c96a038c8bbcaddd547950
Furkan Türkyaşar <turkyasarfurkan@gmail.com>
2024-01-21 22:23:27 +0300

    first commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Furkan Türkyaşar on 18.01.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Furkan Türkyaşar on 21.01.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
